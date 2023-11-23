1b1c90a5b0f4c0cefbf3323d8a5008a5a1a7024c
MambaBY <mambych@gmail.com>
2023-11-23 20:08:55 +0100

    Added new lokators to MyInfo class

--- src/main/java/pages/myinfo/MyInfoPage.java
+++ src/main/java/pages/myinfo/MyInfoPage.java
@@ -10,7 +10,14 @@ public class MyInfoPage extends BasePage {
     public static final SelenideElement title = $x("//h6[text() = 'Personal Details']");
     public static final SelenideElement firstName = $x("//input[@name='firstName']");
     public static final SelenideElement lastName = $x("//input[@name='lastName']");
-
+    public static final SelenideElement userName =
+            $x("(//input[@class = 'oxd-input oxd-input--active'])[2]");
+    public static final SelenideElement driverLicenseNumber =
+            $x("(//input[@class = 'oxd-input oxd-input--active'])[5]");
+    public static final SelenideElement driverLicenseExpiryDate =
+            $x("(//input[@class = 'oxd-input oxd-input--active'])[6]");
+    public static final SelenideElement nationalitySelector =
+            $x("  (//div[@class='oxd-select-text-input'])[1]");
 
 
     public MyInfoPage checkTheTitle() {
